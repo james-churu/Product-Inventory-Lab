b68d189b1c52381708ea923e166ddefbe1ac718f
jchuru <jameschuru123@gmail.com>
2020-02-13 19:01:54 -0500

    Added instance variables for candy

--- src/main/java/Models/Candy.java
+++ src/main/java/Models/Candy.java
@@ -1,4 +1,19 @@
 package Models;
 
 public class Candy {
+    double price;
+    String name;
+    int ID;
+    String packaging;
+    boolean hardCandy;
+    double price;
+    int QTY;
+
+    public void Candy(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
 }
